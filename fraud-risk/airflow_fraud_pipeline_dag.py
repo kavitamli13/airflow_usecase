c7f6c27dd7a0d1ff12d8d3d113814e9c51110926
kavitamli13 <kavithanjalitamilselvan@gmail.com>
2026-08-19 20:55:20 +0530

    Update airflow_fraud_pipeline_dag.py

--- fraud-risk/airflow_fraud_pipeline_dag.py
+++ fraud-risk/airflow_fraud_pipeline_dag.py
@@ -215,6 +215,7 @@ def _train_model_via_spark(**context):
         "--master", "k8s://https://kubernetes.default.svc.cluster.local.",
         "--deploy-mode", "client",
         "--conf", "spark.kubernetes.namespace=data-platform",
+        "--conf", "spark.submit.pyModules=joblib,scikit-learn,pandas",
         training_script_path,  # PySpark script path (local, HDFS, or https://)
         TRAINING_SNAPSHOT_PATH,     # arg 1: HDFS input
         MODEL_CANDIDATE_PATH,        # arg 2: model output
